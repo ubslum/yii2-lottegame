03a72ee6197af39e127c4555364bac1c5652fb14
ubslum <shinichi7586@yahoo.com.vn>
2018-06-05 15:58:02 +0700

    Add files via upload

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ubslum\lottegame;
+
+/**
+ * lottegame module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'ubslum\lottegame\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
